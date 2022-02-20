f01a3921b44c61b453e36391e3b226dfe898652b
emdg05 <ed_139@usc.edu>
2022-02-20 01:10:17 -0800

    redirect

--- app.js
+++ app.js
@@ -30,7 +30,7 @@
 
  var client_id = 'd6cc8aeb975e401b9a736b0a64ae9f48'; // Your client id
  var client_secret = 'aad2cb7f1c8d41c396b4f9c28ccfed66'; // Your secret
- var redirect_uri = 'http://spotifusion.herokuapp.com/'; // Your redirect uri
+ var redirect_uri = 'http://spotifusion.herokuapp.com/createjoin/'; // Your redirect uri
  var scopes = ['user-read-private', 'user-read-email', 'user-top-read', 'playlist-modify-public', 'playlist-read-collaborative', 'playlist-read-private', 'user-library-read'],
      state = 'spotify_auth_state';
  var mySpotifyApi = new SpotifyWebApi({
